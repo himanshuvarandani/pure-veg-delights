c23acf9f271443fecbb960f2b7154312a985da2c
Himanshu <himvarandani@gmail.com>
2024-02-09 16:11:17 +0530

    Add search bar to products page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,27 +1,42 @@
 import ProductsList from "@/components/ProductsList"
+import SearchBar from "@/components/SearchBar"
 
 const Search = () => {
   return (
     <div className="py-10">
+      <div className="flex justify-center pb-5">
+        <SearchBar />
+      </div>
+
       <div className="pb-10 px-5 sm:px-10 lg:px-20">
-        <h3 className="text-2xl text-orange-550 text-center font-bold mb-2">Breakfast</h3>
-        <ProductsList theme="white" />
+        <h3 className="text-2xl text-orange-550 font-bold mb-2">Breakfast</h3>
+        <div className="md:px-5">
+          <ProductsList theme="white" />
+        </div>
       </div>
       <div className="pb-10 px-5 sm:px-10 lg:px-20">
-        <h3 className="text-2xl text-orange-550 text-center font-bold mb-2">Meals</h3>
-        <ProductsList theme="white" />
+        <h3 className="text-2xl text-orange-550 font-bold mb-2">Meals</h3>
+        <div className="md:px-5">
+          <ProductsList theme="white" />
+        </div>
       </div>
       <div className="pb-10 px-5 sm:px-10 lg:px-20">
-        <h3 className="text-2xl text-orange-550 text-center font-bold mb-2">Snacks</h3>
-        <ProductsList theme="white" />
+        <h3 className="text-2xl text-orange-550 font-bold mb-2">Snacks</h3>
+        <div className="md:px-5">
+          <ProductsList theme="white" />
+        </div>
       </div>
       <div className="pb-10 px-5 sm:px-10 lg:px-20">
-        <h3 className="text-2xl text-orange-550 text-center font-bold mb-2">Rice</h3>
-        <ProductsList theme="white" />
+        <h3 className="text-2xl text-orange-550 font-bold mb-2">Rice</h3>
+        <div className="md:px-5">
+          <ProductsList theme="white" />
+        </div>
       </div>
       <div className="pb-10 px-5 sm:px-10 lg:px-20">
-        <h3 className="text-2xl text-orange-550 text-center font-bold mb-2">Breads</h3>
-        <ProductsList theme="white" />
+        <h3 className="text-2xl text-orange-550 font-bold mb-2">Breads</h3>
+        <div className="md:px-5">
+          <ProductsList theme="white" />
+        </div>
       </div>
 
       <div className="bg-orange-500 py-10 px-5 sm:px-10 lg:px-20">
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { faSearch } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
+import { useDebouncedCallback } from "use-debounce"
+
+const SearchBar = () => {
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const { replace } = useRouter()
+
+  const handleSearch = useDebouncedCallback((term: string) => {
+    if (term) {
+      replace(`${pathname}?q=${term}`)
+    } else {
+      replace(`${pathname}`)
+    }
+  }, 500)
+
+  return (
+    <div className="flex border-2 border-orange-550 rounded-3xl overflow-hidden pl-2 mx-2">
+      <input
+        className="bg-transparent text-orange-550 placeholder-orange-550 my-1 mx-2 p-1"
+        placeholder="Search a product ..."
+        defaultValue={searchParams.get("q")?.toString()}
+        onChange={(e) => handleSearch(e.target.value)}
+      />
+      <button className="bg-orange-550 flex items-center text-orange-600 px-2">
+        <FontAwesomeIcon icon={faSearch} height={15} className="text-white" />
+      </button>
+    </div>
+  )
+}
+
+export default SearchBar
