f6efa4ff22fb036244922ba3f8b0d2cce2fde9dd
Himanshu <himvarandani@gmail.com>
2024-02-19 02:13:52 +0530

    Update components

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,4 +1,4 @@
-import ProductCard from "./ProductCard"
+import Image from "next/image"
 
 type PropsType = {
   classNames?: string
@@ -10,7 +10,41 @@ const ProductsList = ({ classNames, theme, products=[] }: PropsType) => {
   return (
     <div className={`flex flex-wrap ${classNames}`}>
       {products.map((product: Product) => (
-        <ProductCard theme={theme} product={product} key={product.name} />
+        <div 
+          className={`border-2 rounded-xl overflow-hidden flex sm:flex-col sm:justify-between w-full sm:w-60 m-2
+            ${theme === "white" ? "border-yellow-500" : "border-white"}
+          `}
+          key={product.name}
+        >
+          <div className="relative h-full w-20 xs:w-28 sm:w-full sm:h-40">
+            <Image
+              src={product.image}
+              alt="Food Image"
+              layout="fill"
+              objectFit="cover"
+              quality={100}
+            />
+          </div>
+          <div className="flex flex-1 flex-col justify-between py-2 sm:py-5 px-3">
+            <h5 className="text-sm sm:text-lg sm:mb-2">{product.name}</h5>
+            <div>
+              <p
+                className={`text-lg sm:text-2xl font-bold mb-2 sm:mb-4
+                  ${theme === "white" ? "text-orange-550" : "text-white"}
+                `}
+              >
+                Rs. {product.price}/-
+              </p>
+              <button
+                className={`w-full rounded-2xl text-sm sm:text-base text-center px-4 py-2
+                  ${theme === "white" ? "bg-orange-550" : "bg-white"}
+                `}
+              >
+                Add to Cart
+              </button>
+            </div>
+          </div>
+        </div>
       ))}
     </div>
   )
--- src/components/SearchProductsList.tsx
+++ src/components/SearchProductsList.tsx
@@ -17,14 +17,21 @@ const SearchProductsList = () => {
 
   return (
     <div>
-      {Object.keys(products).map((category) => (
-        <div className="pb-10 px-2 xs:px-5 sm:px-10 lg:px-20" key={category}>
-          <h3 className="text-2xl text-orange-550 font-bold mb-2">{category}</h3>
-          <div className="md:px-5">
-            <ProductsList theme="white" products={products[category]} />
+      {!Object.keys(products).length
+        ? (
+          <p className="py-10 text-center text-orange-550">
+            <span className="text-lg font-bold">Sorry!!</span>
+            &nbsp;No Products Available for this search query.
+          </p>
+        ) : Object.keys(products).map((category) => (
+          <div className="pb-10 px-2 xs:px-5 sm:px-10 lg:px-20" key={category}>
+            <h3 className="text-2xl text-orange-550 font-bold mb-2">{category}</h3>
+            <div className="md:px-5">
+              <ProductsList theme="white" products={products[category]} />
+            </div>
           </div>
-        </div>
-      ))}
+        ))
+      }
     </div>
   )
 }
